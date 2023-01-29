c21326de77dbcabe19cf36dd891155b2e0c53c95
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-29 15:55:39 +0500

    tag increment

--- Sources/FoodLabelExtractor/Views/ExtractorView/ExtractorView+ImageViewer.swift
+++ Sources/FoodLabelExtractor/Views/ExtractorView/ExtractorView+ImageViewer.swift
@@ -33,8 +33,8 @@ extension ExtractorView {
         
         return ImageViewer(viewModel: imageViewerViewModel)
             .overlay(overlay)
-//            .opacity(extractor.dismissState.shouldShrinkImage ? 0 : 1)
-            .offset(y: extractor.dismissState.shouldShrinkImage ? UIScreen.main.bounds.height + extractor.imageOffset : 0)
+            .opacity(extractor.dismissState.shouldShrinkImage ? 0 : 1)
+//            .offset(y: extractor.dismissState.shouldShrinkImage ? UIScreen.main.bounds.height + extractor.imageOffset : 0)
 //            .scaleEffect(extractor.dismissState.shouldShrinkImage ? 0 : 1)
 //            .padding(.top, extractor.dismissState.shouldShrinkImage ? 400 : 0)
 //            .padding(.trailing, extractor.dismissState.shouldShrinkImage ? 300 : 0)
