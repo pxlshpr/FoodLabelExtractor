2cc2a2af3462864400dd86642444a92026367d90
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-29 13:30:26 +0500

    tag increment

--- Sources/FoodLabelExtractor/Extractor/Extractor.swift
+++ Sources/FoodLabelExtractor/Extractor/Extractor.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import VisionSugar
 import FoodLabelScanner
 import PrepDataTypes
+import SwiftHaptics
 
 @MainActor
 public class Extractor: ObservableObject {
@@ -215,55 +216,24 @@ extension Extractor {
     }
 
     func handleDeletedNutrient(oldValue: [ExtractedNutrient]) {
-        //TODO: Uncomment all this code
-//        Haptics.warningFeedback()
-//        /// get the index of the deleted attribute
-//        var deletedIndex: Int? = nil
-//        for index in oldValue.indices {
-//            if !scannerNutrients.contains(where: { $0.attribute == oldValue[index].attribute }) {
-//                deletedIndex = index
-//            }
-//        }
-//        guard let deletedIndex else { return }
-//
-//        let shouldUnsetCurrentAttributeUponCompletion: Bool
-//        if self.currentAttribute == oldValue[deletedIndex].attribute {
-//            self.currentAttribute = nil
-//            shouldUnsetCurrentAttributeUponCompletion = true
-//        } else {
-//            shouldUnsetCurrentAttributeUponCompletion = false
-//        }
-//        checkIfAllNutrientsAreConfirmed(unsettingCurrentAttribute: shouldUnsetCurrentAttributeUponCompletion)
-//
-////        /// now if the deleted nutrient was the current one, select the next unconfirmed or next-inline attribute to it
-////        func moveToNextAttributeIfCurrentWasDeleted() {
-////            guard let currentAttribute else { return }
-////            guard oldValue[deletedIndex].attribute == currentAttribute else {
-////                return
-////            }
-////            if deletedIndex - 1 < scannerNutrients.count, deletedIndex - 1 >= 0 {
-////                if let nextAttributeToDeletedOne = nextUnconfirmedAttribute(to: scannerNutrients[deletedIndex - 1].attribute) {
-////                    /// first see if an unconfirmed item exists before the (old, deleted) one, and if so, move to that
-////                    moveToAttribute(nextAttributeToDeletedOne)
-////                } else {
-////                    /// otherwise move to the
-////                    moveToAttribute(scannerNutrients[deletedIndex - 1].attribute)
-////                }
-////            } else {
-////                guard !scannerNutrients.isEmpty else {
-////                    return
-////                }
-////                if !scannerNutrients[0].isConfirmed {
-////                    moveToAttribute(scannerNutrients[0].attribute)
-////                } else if let nextAttributeToFirstOne = nextUnconfirmedAttribute(to: scannerNutrients[0].attribute) {
-////                    /// otherwise, we either deleted the first item or have none remaining, so move to the first one if it exists
-////                    moveToAttribute(nextAttributeToFirstOne)
-////                } else if !scannerNutrients.isEmpty {
-////                    moveToAttribute(scannerNutrients[0].attribute)
-////                }
-////            }
-////        }
-////
-////        moveToNextAttributeIfCurrentWasDeleted()
+        
+        Haptics.warningFeedback()
+        /// get the index of the deleted attribute
+        var deletedIndex: Int? = nil
+        for index in oldValue.indices {
+            if !extractedNutrients.contains(where: { $0.attribute == oldValue[index].attribute }) {
+                deletedIndex = index
+            }
+        }
+        guard let deletedIndex else { return }
+
+        let shouldUnsetCurrentAttributeUponCompletion: Bool
+        if self.currentAttribute == oldValue[deletedIndex].attribute {
+            self.currentAttribute = nil
+            shouldUnsetCurrentAttributeUponCompletion = true
+        } else {
+            shouldUnsetCurrentAttributeUponCompletion = false
+        }
+        checkIfAllNutrientsAreConfirmed(unsettingCurrentAttribute: shouldUnsetCurrentAttributeUponCompletion)
     }
 }
